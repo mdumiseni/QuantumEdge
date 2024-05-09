7f4b85ee231bda6d97ec1b4966fdc4413e3fc406
Mdumiseni <47352274+mdumiseni@users.noreply.github.com>
2024-05-10 00:12:51 +0200

    Checkpoint
    - Added UI Template.
    - Added Echarts.js for creating interesting charts

---
+++ QuantumEdge/QuantumEdge.com/Data/QuantumEdgecomContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using QuantumEdge.com.Data;
+
+namespace QuantumEdge.com.Data
+{
+    public class QuantumEdgecomContext(DbContextOptions<QuantumEdgecomContext> options) : IdentityDbContext<QuantumEdgecomUser>(options)
+    {
+    }
+}
